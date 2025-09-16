e7a7c88ca45dc04dfac939da196c017d46dce7ba
aayushwrld <aayushh2k@gmail.com>
2025-09-16 09:59:34 +0530

    swagger ui + nodemon setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,16 +1,19 @@
 import express from 'express';
 import { userRouter } from './routes/user.routes';
+import swaggerUI from 'swagger-ui-express';
+import swaggerSpec from './swagger';
 
 const app = express();
 const PORT = 3000;
 
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerSpec));
 app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send("Welcome to the server!");
 });
 
-app.use('/users', userRouter)
+app.use('/users', userRouter);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
