b7732c82f2b0520d8668ef92f9765f92eb4f4e83
Rajab Shoukath Kunnath <rajabshoukathis@gmail.com>
2025-09-25 16:06:21 +0530

    Add Toast component and spinners; update dependencies

--- waste-management/app/layout.tsx
+++ waste-management/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Toast } from "@/components/ui/toast";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,13 +26,14 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider>
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
+      <html lang="en">
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+          {children}
+          <Toast theme="light" />
+        </body>
+      </html>
     </ClerkProvider>
   );
 }
